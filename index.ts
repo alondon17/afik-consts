eab7442310e0a0e1f7dfd4c636e89796069dd598
alondon17 <alonaloneast@gmail.com>
2021-04-12 16:10:21 +0300

    base in commonjs

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
 // import qualityCategories from "./data/qualityCategories";
 
-export {default as qualityCategories} from './data/qualityCategories'
-export {default as scores} from './data/scores'
-export {default as professions} from './data/professions'
-export {default as odds} from './data/odds'
+// export {default as qualityCategories} from './data/qualityCategories'
+// export {default as scores} from './data/scores'
+// export {default as professions} from './data/professions'
+// export {default as odds} from './data/odds'
+import odds from './data/odds'
+import professions from './data/professions'
+import qualityCategories from './data/qualityCategories'
+import scores from './data/scores'
+export={
+professions,
+scores,
+qualityCategories,
+odds
+}
 // module.exports={
 //     qualityCategories:require('./data/qualityCategories'),
 // scores: require('./data/scores'),
