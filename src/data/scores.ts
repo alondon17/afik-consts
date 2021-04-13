c3be8e80368ee62d7dba818c33c9e3a82769b876
alondon17 <alonaloneast@gmail.com>
2021-04-13 10:07:36 +0300

    working with tsc

--- src/data/scores.ts
+++ src/data/scores.ts
@@ -6,6 +6,6 @@ const scores = {
     7: '7',
     8: '8',
     9: '9',
-    10: '10'
+    10: '10',
 }
 export default scores
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+// import qualityCategories from "./data/qualityCategories";
+
+import qualityCategories from './data/qualityCategories'
+import odds from './data/odds'
+import professions from './data/professions'
+import scores from './data/scores'
+export={ qualityCategories,odds,professions,scores}
+// export {default as scores} from './data/scores'
+// export {default as professions} from './data/professions'
+// export {default as qualityCategories} from './data/qualityCategories'
+// export {default as scores} from './data/scores'
+// export {default as professions} from './data/professions'
+// export {default as odds} from './data/odds'
+// const odds =require ('./data/odds')
+// const professions =require ('./data/professions')
+// const qualityCategories =require ('./data/qualityCategories')
+// const scores =require ('./data/scores')
+// module.exports={odds,professions,qualityCategories,scores}
+// module.exports={
+//     qualityCategories:require('./data/qualityCategories'),
+// scores: require('./data/scores'),
+// professions:require('./data/professions'),
+// odds:require ('./data/odds')
+// }
